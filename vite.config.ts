0b9d513a47c8b2f7abb8b47fd6df3af5ebb44fe0
Shashankredddy <43519932+Shashankredddy@users.noreply.github.com>
2025-09-05 15:51:33 -0400

    Updated base URL in vite.config.ts and rebuilt project for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
   base: "/",
-=======
-  base: "/portfolio/",
->>>>>>> c5304cbdac1a9b0d247e214bc8f4ad46a8366dd0
 });
